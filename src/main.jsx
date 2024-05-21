c8bda01baa73375fb88414bcf03bcc34ab09de62
Rifat Rahman <rifat.rahman378@gmail.com>
2024-05-22 00:31:03 +0600

    pages

--- src/main.jsx
+++ src/main.jsx
@@ -10,19 +10,29 @@ import Root from './components/root/Root';
 import Home from './components/home/Home';
 import Appliedjobs from './components/Pages/Appliedjobs';
 import Errorpage from './components/Pages/Errorpage';
+import Statistics from './components/Pages/Statistics';
+import Blog from './components/Pages/Blog';
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <Errorpage></Errorpage>,
+    errorElement: <Errorpage/>,
     children: [
       {
         path: "/",
         element: <Home />
       },
       {
+        path:"/statistics",
+        element: <Statistics/>
+      },
+      {
         path: "/applied-jobs",
         element: <Appliedjobs />
+      },
+      {
+        path: "/blog",
+        element: <Blog/>
       }
     ]
   },
